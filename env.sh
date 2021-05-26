62831128334e6c93c16291b3d7321dee3b94cb0a
Yuan Mei <yuan.mei@gmail.com>
2021-05-26 00:32:16 -0700

    Add netgen, xschem, gaw, ngspice, verilator and clean up Makefile.

--- env.sh
+++ env.sh
@@ -1,4 +1,5 @@
 PREFIX=/opt/OpenICEDA
+export VERILATOR_ROOT=$PREFIX/src/verilator
 export PATH=$PREFIX/bin:$PATH
 export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
 export MANPATH=$PREFIX/share/man:$MANPATH
