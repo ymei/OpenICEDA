cb3608e574e0653bb43d8d9cc102a9f99ef19e23
Yuan Mei <yuan.mei@gmail.com>
2021-06-08 05:49:56 -0700

    Add KLayout.

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 PREFIX=/opt/OpenICEDA
 export VERILATOR_ROOT=$PREFIX/src/verilator
-export PATH=$PREFIX/bin:$PATH
-export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+export PATH=$PREFIX/bin:$PREFIX/src/klayout/bin-release:$PATH
+export LD_LIBRARY_PATH=$PREFIX/lib:$PREFIX/src/klayout/bin-release:$LD_LIBRARY_PATH
 export MANPATH=$PREFIX/share/man:$MANPATH
