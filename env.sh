4761924470e71f6a282714699bca80538727d4d7
Yuan Mei <yuan.mei@gmail.com>
2023-11-23 21:44:28 -0800

    Add OpenRAM

--- env.sh
+++ env.sh
@@ -5,6 +5,9 @@ test -r ${PYVENV}/bin/activate && . ${PYVENV}/bin/activate
 export PDK_ROOT=${PREFIX}/share/pdk
 export VERILATOR_ROOT=${PREFIX}/src/verilator
 export OPENLANE_ROOT=${PREFIX}/src/OpenLane
+export OPENRAM_HOME=${PREFIX}/src/OpenRAM/compiler
+export OPENRAM_TECH=${PREFIX}/src/OpenRAM/technology
+export PYTHONPATH=${PYTHONPATH}:${OPENRAM_HOME}
 export PATH=${PREFIX}/bin:${PREFIX}/src/klayout/bin-release:${PATH}
 export LD_LIBRARY_PATH=${PREFIX}/lib:${PREFIX}/src/klayout/bin-release:${LD_LIBRARY_PATH}
 export MANPATH=${PREFIX}/share/man:${MANPATH}
